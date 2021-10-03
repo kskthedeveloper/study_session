a5c89d09ad1281e09af800cc0ca390feeadbaca8
Kaung San Kyaw <kskthedeveloper@gmail.com>
2021-10-03 12:32:15 +0630

    disjkstra twro stack algorithm for expression evaluation

---
+++ algorithms-robert-sedgewick/src/DijkstraTwoStackAlgorithm.java
@@ -0,0 +1,54 @@
+import edu.princeton.cs.algs4.StdIn;
+
+import java.util.Stack;
+
+public class DijkstraTwoStackAlgorithm {
+    public static void main(String[] args) {
+        Stack<String> operators = new Stack<String>();
+        Stack<Double> values = new Stack<Double>();
+
+        System.out.println(StdIn.isEmpty());
+
+        // TODO: to check the loop is not existing
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+
+            if (s.equals("(")) {
+            } else if (s.equals("+")) {
+                operators.push(s);
+            } else if (s.equals("-")) {
+                operators.push(s);
+            } else if (s.equals("*")) {
+                operators.push(s);
+            } else if (s.equals("/")) {
+                operators.push(s);
+            } else if (s.equals("sqrt")) {
+                operators.push(s);
+            } else if (s.equals(")")) {
+                String operator = operators.pop();
+                double value = values.pop();
+
+                if (operator.equals("+")) {
+                    value = values.pop() + value;
+                }
+                else if (operator.equals("-")) {
+                    value = values.pop() - value;
+                }
+                else if (operator.equals("*")) {
+                    value = values.pop() * value;
+                }
+                else if (operator.equals("/")) {
+                    value = values.pop() / value;
+                }
+                else if (operator.equals("sqrt")) {
+                    value = Math.sqrt(value);
+                }
+
+                System.out.println(value);
+                values.push(value);
+            } else {
+                values.push(Double.parseDouble(s));
+            }
+        }
+    }
+}
