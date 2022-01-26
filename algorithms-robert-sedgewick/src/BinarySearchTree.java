91cd3bf00a5c476b02861d0a4afea5c0e13304e6
Kaung San Kyaw <kskthedeveloper@gmail.com>
2022-01-26 21:55:43 +0630

    testing inserting

--- algorithms-robert-sedgewick/src/BinarySearchTree.java
+++ algorithms-robert-sedgewick/src/BinarySearchTree.java
@@ -1,6 +1,15 @@
 public class BinarySearchTree {
     public static void main(String[] args) {
         Tree tree = new Tree();
+        tree.insert(5);
+        tree.insert(4);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(7);
+        tree.insert(8);
+        tree.insert(9);
         tree.insert(10);
         System.out.println(tree);
     }
@@ -20,8 +29,8 @@ class Tree {
         public String toString() {
             return "Node{" +
                     "value=" + value +
-//                    ", leftChild=" + leftChild +
-//                    ", rightChild=" + rightChild +
+                    ", leftChild=" + leftChild +
+                    ", rightChild=" + rightChild +
                     '}';
         }
     }
